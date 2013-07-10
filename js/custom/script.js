ebe6299a2e663a844bee5ed124ecbcd06b3523f0
Aayush Iyer <aayushiyer@Aayushs-MacBook-Pro-2.local>
2013-07-10 15:06:33 -0600

    Nav locked in

--- js/custom/script.js
+++ js/custom/script.js
@@ -2,11 +2,27 @@
  * scripts.js
  */
 (function($) {
-$('.portfolio').masonry({
-    columnWidth: 300,
-    gutter: 10,
-    itemSelector: '.item'
+$(document).ready(function(){
+  $('.portfolio').masonry({
+      columnWidth: 300,
+      gutter: 10,
+      itemSelector: '.item'
+  });
 });
+
+
+function jump(dradis){
+  $('html,body').animate({
+    scrollTop: $("#"+dradis).offset().top},
+    '500', function(){
+  });
+}
+
+$('#top nav ul li').click(function(){
+  var coord= $(this).html();
+  jump(coord);
+});
+
 }(jQuery));
 
 
