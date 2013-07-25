a535b59c54c3a357229ab659a27cebb3bcb9b100
Aayush Iyer <aayushiyer@Aayushs-MacBook-Pro-2.local>
2013-07-24 18:28:21 -0600

    Push to other comp

--- js/custom/script.js
+++ js/custom/script.js
@@ -2,26 +2,29 @@
  * scripts.js
  */
 (function($) {
-$(document).ready(function(){
-  $('.portfolio').masonry({
-      columnWidth: 300,
-      gutter: 10,
-      itemSelector: '.item'
+  
+  function jump(dradis){
+    $('html,body').animate({
+      scrollTop: $("#"+dradis).offset().top},
+      '500', function(){
+    });
+  }
+
+  $(document).ready(function(){
+    jump("work");
+    $('.portfolio').masonry({
+        columnWidth: 300,
+        gutter: 10,
+        itemSelector: '.item'
+    });
   });
-});
 
 
-function jump(dradis){
-  $('html,body').animate({
-    scrollTop: $("#"+dradis).offset().top},
-    '500', function(){
+
+  $('#top nav ul li').click(function(){
+    var coord= $(this).html();
+    jump(coord);
   });
-}
-
-$('#top nav ul li').click(function(){
-  var coord= $(this).html();
-  jump(coord);
-});
 
 }(jQuery));
 
